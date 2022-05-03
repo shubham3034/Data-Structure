f48e629b7e84806199f7605880a941ccd055f3e6
Shubham <101803743+shubham3034@users.noreply.github.com>
2022-05-03 13:19:57 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10],i;
+	printf("SHUBHAM\n");
+	for(i=0;i<10;i++)
+	{
+	printf("Enter the element of array=");
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+	printf("%d\t",a[i]);
+	}
+	getch();
+}
